368dfd11a79a7fac2bd1f8d9a895e42dccb96bed
hopeyoungblood <85723621+hopeyoungblood@users.noreply.github.com>
2021-07-02 17:30:43 -0700

    PyPoll vF1

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,12 +4,16 @@ election_data = "../Resources/election_data.csv"
 
 f = open("PyPoll.txt", "w")
 
-candidate_list = {}
 total_votes = 0
 candidate_name = 0
 percentage_won = 0
 votes_won = 0
 winner = 0
+most_votes = 0
+candidates = {}
+# candidates = dict()
+# candidates = {candidate_name, votes_won, percentage_won}
+
 
 with open(election_data,'r') as csvfile:
 
@@ -26,14 +30,19 @@ with open(election_data,'r') as csvfile:
       candidate_name = row[2]
 
       total_votes = total_votes + 1
+      
+      # percentage_won = percentage_won + 1
+      percentage_won = votes_won / total_votes
+      
+      most_votes = votes_won + 1
 
-        #   percentage_won = percentage_won + 1
-
-      if candidate_name in candidate_list:
-        candidate_list[candidate_name] = candidate_list[candidate_name] + 1
+      if candidate_name in candidates:
+        candidates[candidate_name] = candidates[candidate_name] + 1
       else:
-        candidate_list[candidate_name] = 1
-    print(candidate_list)
+        candidates[candidate_name] = 1
+    print(candidates)
+
+    # candidates = dict(candidate_name, votes_won, percentage_won)
 
 file_name = "PyPoll.txt"
  
@@ -45,19 +54,15 @@ with open(file_name, "a") as txt_file:
     txt_file.write(f"Total Votes {total_votes}\n")
     txt_file.write("---------------\n")
    
-    for k, v, in candidate_list.items():
+    for k, v, in candidates.items():
         txt_file.write(str(k) + ": " + str(v) + "\n\n")
+  
+    txt_file.write("---------------\n")
 
-        #    txt_file.write(f"Khan: {percentage_won}\n {votes_won}\n")
+    # for k, v, in most_votes.items():
+    #   txt_file.write(str(k) + ": " + str(v) + "\n\n")
 
-        #    txt_file.write(f"Correy: {percentage_won}\n {votes_won}\n")
-
-        #    txt_file.write(f"Li: {percentage_won}\n {votes_won}\n")
-
-        #    txt_file.write(f"O'Tooley: {percentage_won}\n {votes_won}\n")
-        #    txt_file.write("---------------\n")
-        
-        #    txt_file.write(f"Winner: {winner}\n")
+    txt_file.write(f"Winner: {winner}\n")
  
 file_name = "PyPoll.txt"
 
