303de93ff84f6e8585a1ca1a95a3e809f5b4c9a5
hopeyoungblood <85723621+hopeyoungblood@users.noreply.github.com>
2021-07-03 15:42:53 -0700

    PyBank vSa1

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,6 +10,7 @@ last = 0
 months = 0
 total_change = 0
 total_months = 0
+num_months = []
 profits_losses = []
 average_change = 0
 greatest_increase = 0
@@ -26,8 +27,8 @@ with open(budget_data,'r') as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
-    profits_losses.append(total_change)
-    print(profits_losses)
+    # profits_losses.append(total_change)
+    # print(profits_losses)
 
     # Read each row of data after the header
     for row in csvreader:
@@ -39,8 +40,11 @@ with open(budget_data,'r') as csvfile:
         total_change = total_change + int(row[1])
         print(total_change)
 
-        # average_change = (last - first) / (total_months - 1)
-        # print(average_change)
+        average_change = (last - first) / total_months
+        print(average_change)
+
+        # profits_losses.append(total_change)
+        # print(profits_losses)
 
         # # # step two just find the difference between the two months
         current = int(row[1])
@@ -55,23 +59,21 @@ with open(budget_data,'r') as csvfile:
             total_change = total_change + change
             print(total_change)
 
-            last = int(row[1])
-            print(last)
-
-            average_change = (last - first) / (total_months - 1)
+            average_change = (last - first) / (total_months + 1)
             print(average_change)
 
-            # average_change = (last - first) / (total_months - 1)
-            # print(average_change)
+        last = int(row[1])
+        print(last)
+
+        # num_months.append = total_months
+        # print(num_months)
+
+        # profits_losses.append(total_change)
+        # print(profits_losses)
             
-            # greatest_increase = 
-            # greatest_decrease = 
+        # greatest_increase = 
+        # greatest_decrease = 
     
-    
-    # profits_losses.append(total_change)
-    # print(profits_losses)
-
-
 file_name = "PyBank.txt"
 
 with open(file_name, "a") as txt_file:
@@ -82,9 +84,9 @@ with open(file_name, "a") as txt_file:
     
     txt_file.write(f"Total Months: {total_months}\n")
     
-    txt_file.write(f"Net Total: {total_change}\n")
+    txt_file.write(f"Net Total: ${total_change}\n")
      
-    txt_file.write(f"Average Change: {average_change}\n")
+    txt_file.write(f"Average Change: ${average_change}\n")
 
     txt_file.write(f"Greatest Increase in Profits: {greatest_increase}\n")
 
