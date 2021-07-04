855b60cca2a0d8b6e5af31c9922dd18507cf5399
hopeyoungblood <85723621+hopeyoungblood@users.noreply.github.com>
2021-07-04 13:48:09 -0700

    PyPoll vSu1

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,7 @@ import csv
 election_data = "../Resources/election_data.csv"
 
 f = open("PyPoll.txt", "w")
-
+ 
 total_votes = 0
 candidate_name = 0
 percentage_won = 0
@@ -11,8 +11,9 @@ votes_won = 0
 winner = 0
 most_votes = 0
 candidates = {}
-# candidates = dict()
 # candidates = {candidate_name, votes_won, percentage_won}
+# candidates = dict()
+# candidates = {"name": candidate_name, "votes won": votes_won, "percentage won": percentage_won}
 
 
 with open(election_data,'r') as csvfile:
@@ -32,7 +33,8 @@ with open(election_data,'r') as csvfile:
       total_votes = total_votes + 1
       
       # percentage_won = percentage_won + 1
-      percentage_won = votes_won / total_votes
+      # percentage_won = votes_won / total_votes
+      # print(percentage_won)
       
       most_votes = votes_won + 1
 
@@ -40,10 +42,18 @@ with open(election_data,'r') as csvfile:
         candidates[candidate_name] = candidates[candidate_name] + 1
       else:
         candidates[candidate_name] = 1
-    print(candidates)
+        print(candidates)
+         
 
-    # candidates = dict(candidate_name, votes_won, percentage_won)
+    for key, value in candidates.items():
+      if value > most_votes:
+        candidate_name = key
+        most_votes = value
+        
+  
+# candidates = dict()
 
+# candidates = {"name": candidate_name), ("vote's won": votes_won), ("percentage won": percentage_won)}
 file_name = "PyPoll.txt"
  
 with open(file_name, "a") as txt_file:
