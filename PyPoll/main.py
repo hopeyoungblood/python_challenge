0003edc5233d444942bc3a761943f2c699c44ec8
hopeyoungblood <85723621+hopeyoungblood@users.noreply.github.com>
2021-07-05 16:44:39 -0700

    PyPoll vM2

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,73 +10,39 @@ percentage_won = 0
 votes_won = 0
 most_votes = 0
 candidates = {}
-candidate_list = []
-
-# candidates = {candidate_name, votes_won, percentage_won}
-# candidates = dict()
-# candidates = {"name": candidate_name, "votes won": votes_won, "percentage won": percentage_won}
-
+# candidate_list = []
 
 with open(election_data,'r') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
-
     
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
-
-    # candidates = {"name": candidate_name, "votes won": votes_won, "percentage won": percentage_won}
-    # def candidates(election_data):
-    # # For readability, it can help to assign your values to variables with descriptive names
-    #   candidate_name = str(row[2])
-    #   votes_won = int(election_data[1]
-      
 
     for row in csvreader:
       
       candidate_name = row[2]
 
       total_votes = total_votes + 1
-      
-      # percentage_won = percentage_won + 1
-      # percentage_won = (votes_won / total_votes) * 100
-      # print(percentage_won)
-      
-      # most_votes = most_votes + 1
 
-      # def candidates{
-      #   candidate_name = row[2],
-      #   votes_won = 
-      #   percentage_won}
+      percentage_won = (votes_won / total_votes) * 100
 
       if candidate_name in candidates:
         candidates[candidate_name] = candidates[candidate_name] + 1
       else:
         candidates[candidate_name] = 1
         print(candidates)
+    
+    for key, value in candidates.items():
+      if value > most_votes:
+       candidate_name = key
+       most_votes = value
         
-        # if votes_won in most_votes:
-        #   most_votes[votes_won] = most_votes[votes_won] + 1
-        # else:
-        #  most_votes[votes_won] = 1
-         
-  #  candidate_list[
-  #    candidate_name  votes_won
-    #  Khan: 2218231
-    # Correy: 704200
-      # Li: 492940
-      # O'Tooley: 105630
-    # for key, value in candidates.items():
-    #   if value > most_votes:
-    #     candidate_name = key
-    #     most_votes = value
-        
-# candidates = dict()
 
-# candidates = {"name": candidate_name), ("vote's won": votes_won), ("percentage won": percentage_won)}
+
 file_name = "PyPoll.txt"
  
 with open(file_name, "a") as txt_file:
@@ -92,10 +58,6 @@ with open(file_name, "a") as txt_file:
   
     txt_file.write("---------------\n")
 
-    # for k, v, in most_votes.items():
-    #    txt_file.write(str(k) + ": " + str(v) + "\n\n")
-
-    txt_file.write(f"Winner: {most_votes}\n")
+    txt_file.write(f"Winner: {candidate_name}\n")
  
 file_name = "PyPoll.txt"
-
